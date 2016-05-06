70cb75091df96290bd115c3b1b6b773a78de6346
rost <rost@ukr.net>
2016-05-06 13:38:31 +0300

    everything is works. But we need to add methods

--- mainRobots.js
+++ mainRobots.js
@@ -20,19 +20,24 @@ Battleground.prototype.randomRobot = function(currentRobot) {
   do {
     robot = getRandom(0, (this.robots.length - 1));
   } while(robot == currentRobot);
-  
-  return robot;
+   return robot;
 }
 
 
-Battleground.prototype.awayDeadRobot = function(robot) { //deleting robots from array if destroyed
-	if (robot.health <= 0) { 
-					console.log('  CRASH!!! - ' + robot.name + ' is destroyed and out of the game');
-					robot.alive = false;
-					// delete bg.robots[i];
-					return countAliveRobots--;
-	}
-}	
+// Battleground.prototype.checkDeadRobots = function(rob, rob2) {
+// 	if (rob.health <= 0) { 
+// 		console.log('  CRASH!!! - ' + rob.name + ' is destroyed and out of the game');
+// 		rob.alive = false;
+// 		// console.log(bg.robots[indexRobot].alive);
+// 		countAliveRobots--;
+// 		if (countAliveRobots === 1) {
+// 		console.log('--------- ' + rob2.name + ' WINS!!!! -----------')
+// 		exitLoop = false;
+// 		break;
+// 		}
+// 	}
+// }	
+
 
 // ROBOT
 	function Robot(name) {
@@ -74,24 +79,29 @@ function makeBattle(robotCount) {
 	var healthRobot;
 	var exitLoop = true; // condition of exit from the loop battle
 	var countAliveRobots;
+	var indexRobot;
 	
 	for (var i = 0; i < robotCount; i++) { //create array with robots
 		newRobot = new Robot('Rob_' + (i+1));
 		bg.addRobot(newRobot);
 	}
 	countAliveRobots = bg.robots.length;
-
+	console.log('On the BATTLEGROUND are ' + bg.robots.length + ' robots!');
 	while (exitLoop) {
-		for (var i = 0; i < countAliveRobots; i++) { //begining the battle
-			console.log('ROUND ' + (round));
-			if (bg.robots[i].alive === true) {
-				console.log(' ' + bg.robots[i].name + ' is attack with ' + bg.robots[i].weapon.power + 
-							' power ' + bg.robots[i].weapon.type + ' damage!');
-				var currentRobot = bg.robots[i].name;
-			
+		for (var i = 0; i < bg.robots.length; i++) { //begining the battle
+			if (bg.robots[i].alive == true) {
 
+				console.log('ROUND ' + round + '. FIGHT!!!');
+				console.log(' ' + bg.robots[i].name + ' is ATTACK with ' + bg.robots[i].weapon.power + 
+							' power ' + bg.robots[i].weapon.type + ' damage!');
+				
 				if (bg.robots[i].weapon.type === 'single') { //single damage
-					var indexRobot = bg.randomRobot(i); //index number of random robot
+					do {
+						indexRobot = bg.randomRobot(i); //index number of random robot
+					} while (bg.robots[indexRobot].alive == false);
+					
+					console.log(' ' + bg.robots[indexRobot].name + ' receives ' + bg.robots[i].weapon.power + 
+										' points damage');
 					console.log('  health of ' + bg.robots[indexRobot].name + ' before damage ' + 
 										'= ' + bg.robots[indexRobot].health);
 					bg.robots[indexRobot].health = bg.robots[indexRobot].health -
@@ -99,45 +109,54 @@ function makeBattle(robotCount) {
 					console.log('  health of ' + bg.robots[indexRobot].name + ' after damage ' +
 									 '= ' + bg.robots[indexRobot].health);
 
-					if (bg.robots[i].health <= 0) { 
-							console.log('  CRASH!!! - ' + bg.robots[i].name + ' is destroyed and out of the game');
-							bg.robots[i].alive = false;
-							countAliveRobots--;
-					}
-					if (countAliveRobots === 1) {
-						console.log(bg.robots[i].name + ' WINS!!!!')
+							
+					// bg.checkDeadRobots(bg.robots[indexRobot], bg.robots[i]);				
+					if (bg.robots[indexRobot].health <= 0) { 
+						console.log('  CRASH!!! - ' + bg.robots[indexRobot].name + ' is destroyed and out of the game');
+						bg.robots[indexRobot].alive = false;
+						// console.log(bg.robots[indexRobot].alive);
+						countAliveRobots--;
+						if (countAliveRobots === 1) {
+						console.log('--------- ' + bg.robots[i].name + ' WINS!!!! -----------')
 						exitLoop = false;
+						break;
+						}
 					}
+					
 
 
 				} else { //area damage
-					for (var j = 0; j < countAliveRobots; j++) {
-						if (currentRobot !== bg.robots[j].name && bg.robots[j].alive === true) {
+					for (var j = 0; j < bg.robots.length; j++) {
+						if (bg.robots[i].name !== bg.robots[j].name && bg.robots[j].alive == true) {
 							console.log(' ' + bg.robots[j].name + ' receives ' + bg.robots[i].weapon.power + 
 										' points damage');
 							console.log('  health of ' + bg.robots[j].name + ' before damage ' + 
 											'= ' + bg.robots[j].health);
-						 	// healthRobot = bg.robots[j].health - bg.robots[i].weapon.power;
 						 	bg.robots[j].health = bg.robots[j].health - bg.robots[i].weapon.power;
 							console.log('  health of ' + bg.robots[j].name + ' after damage ' +
 										 '= ' + bg.robots[j].health);
-						}
 						
-
-						if (bg.robots[j].health <= 0) { //deleting robots from array if destroyed
-							console.log('  CRASH!!! - ' + bg.robots[j].name + ' is destroyed and out of the game');
-							bg.robots[j].alive = false;
-							countAliveRobots--;
-						}
-						if (countAliveRobots === 1) {
-							console.log(bg.robots[i].name + ' WINS!!!!')
-							exitLoop = false;
+						
+							if (bg.robots[j].health <= 0) { //deleting robots from array if destroyed
+								console.log('  CRASH!!! - ' + bg.robots[j].name + ' is destroyed and out of the game');
+								bg.robots[j].alive = false;
+								// console.log(bg.robots[indexRobot].alive);
+								countAliveRobots--;
+								if (countAliveRobots === 1) {
+								console.log('--------- ' + bg.robots[i].name + ' WINS!!!! -----------')
+								exitLoop = false;
+								break;
+								}
+							}
 						}
 
 					}
 				}
 				round++;
 			} //if robot alive
+			if (countAliveRobots === 1) {
+				break;
+			}	
 		} // for
 		
 	} // while	
